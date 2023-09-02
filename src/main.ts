a2e53d0971292eec8314ea16a21f3b573f30202b
Mark <mark@remarkablemark.org>
2023-09-02 17:44:40 -0400

    style: fix lint

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import './style.css';
+
+import viteLogo from '/vite.svg';
+
+import { setupCounter } from './counter.ts';
+import typescriptLogo from './typescript.svg';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
@@ -19,6 +21,6 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
       Click on the Vite and TypeScript logos to learn more
     </p>
   </div>
-`
+`;
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+setupCounter(document.querySelector<HTMLButtonElement>('#counter')!);
